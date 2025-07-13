70f33f0f8ced3d0ebc618b092352ce1aa17b42fd
Moe <supremefanatic1@gmail.com>
2025-07-12 20:24:46 -0700

    arithemtic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+fn interproduct(a: i32, b: i32, c: i32) -> i32 {
+    return a * b + b * c + c * a;
+}
+
 fn main() {
-    let x: i32 = 10;
-    println!("x: {x}");
-    // x = 20;
-    // println!("x: {x}");
+    println!("result: {}", interproduct(120, 100, 248));
 }
