96ea0265363bc24b175f834dfa45ba027dbaf116
Moe <supremefanatic1@gmail.com>
2025-07-12 20:25:40 -0700

    fibonacci

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-fn takes_u32(x: u32) {
-    println!("u32: {x}");
-}
-
-fn takes_i8(y: i8) {
-    println!("i8: {y}");
+fn fib(n: u32) -> u32 {
+    if n < 2 {
+        return n;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
 }
 
 fn main() {
-    let x = 10;
-    let y = 20;
-
-    takes_u32(x);
-    takes_i8(y);
-    // takes_u32(y);
+    let n = 20;
+    println!("fib({n}) = {}", fib(n));
 }
