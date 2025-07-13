8db965007809e6930627a4b002e4915c1b0aa2ad
Moe <supremefanatic1@gmail.com>
2025-07-12 20:25:20 -0700

    type inference

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-fn interproduct(a: i32, b: i32, c: i32) -> i32 {
-    return a * b + b * c + c * a;
+fn takes_u32(x: u32) {
+    println!("u32: {x}");
+}
+
+fn takes_i8(y: i8) {
+    println!("i8: {y}");
 }
 
 fn main() {
-    println!("result: {}", interproduct(120, 100, 248));
+    let x = 10;
+    let y = 20;
+
+    takes_u32(x);
+    takes_i8(y);
+    // takes_u32(y);
 }
